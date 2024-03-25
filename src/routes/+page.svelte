979b0fe42b69ef1cc92f9586e07725cfe5eef4ae
James Kopatsis <jam@Meatball.local>
2024-03-25 08:18:24 -0700

    test progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 
 	import Displayer from "../components/Displayer.svelte";
 import Stopwatch from "../components/Stopwatch.svelte";
+	import TestClock from "../components/TestClock.svelte";
 
     let time;
 
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Stopwatch bind:time={time}/>
-<Displayer time={time} />
+<!-- <Stopwatch bind:time={time}/>
+<Displayer time={time} /> -->
+<TestClock />
