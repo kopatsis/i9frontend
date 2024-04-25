0ede6bda948b41eb08b829c3775f2cad9a44da5e
jwork <jkopatsis@gmail.com>
2024-04-24 17:23:22 -0700

    start for lib portion

---
+++ src/lib/stores/library.js
@@ -0,0 +1,5 @@
+import { writable } from "svelte/store";
+
+export const exercises = writable(null);
+export const stretches = writable(null);
+
---
+++ src/routes/library/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import ExerLib from "./ExerLib.svelte";
+    import StrLib from "./StrLib.svelte";
+	let current = 'Exercise';
+</script>
+
+<button
+	on:click={() => {
+		if (current !== 'Exercise') {
+			current = 'Exercise';
+		}
+	}}>{#if current === 'Exercise'}+{/if}Exercises</button
+>
+<button
+	on:click={() => {
+		if (current !== 'Stretch') {
+			current = 'Stretch';
+		}
+	}}
+	>{#if current === 'Stretch'}+{/if}Stretches</button
+>
+
+{#if current === "Exercise"}
+    <ExerLib />
+{:else}
+    <StrLib />
+{/if}
