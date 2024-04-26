6b1da8bb1ae33956de7c6a7820942d016613a120
jwork <jkopatsis@gmail.com>
2024-04-26 12:58:37 -0700

    settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	// @ts-nocheck
+
+	import { getLoginToken } from '$lib/jshelp/localtoken';
+	import { user, getUser } from '$lib/stores/user.js';
+	import { onDestroy, onMount } from 'svelte';
+	import Logout from '../Logout.svelte';
+	import Setting from './Setting.svelte';
+
+	let loading = true;
+	let error = '';
+	let retrievedSettings = null;
+
+	let userData;
+	const unsubscribe = user.subscribe((value) => {
+		userData = value;
+	});
+	onDestroy(unsubscribe);
+
+	onMount(async () => {
+		try {
+			let retset = JSON.parse(localStorage.getItem('yZgvPlBiFb'));
+			if (!retset) {
+				retset = { mode: 'Dark Mode', sound: 'Regular', motion: 'Regular', data: 'Regular' };
+				localStorage.setItem('yZgvPlBiFb', JSON.stringify(retset));
+			}
+			retrievedSettings = retset;
+			const token = getLoginToken();
+			error = await getUser(token);
+		} catch (error) {
+			error = error;
+		}
+		loading = false;
+	});
+</script>
+
+{#if loading}
+	<div>loading...</div>
+{:else if error}
+	<div>F: {error}</div>
+{:else if !userData}
+	<div>Error fetching user data, please try again</div>
+{:else}
+	<Setting key={'mode'} options={['Dark Mode', 'Light Mode']} bind:data={retrievedSettings} />
+	<Setting key={'sound'} options={['Regular', 'Silent']} bind:data={retrievedSettings} />
+	<Setting key={'motion'} options={['Regular', 'Reduced']} bind:data={retrievedSettings} />
+	<Setting key={'data'} options={['Regular', 'Data Saver']} bind:data={retrievedSettings} />
+	{#if userData.Paying}
+		<button>Cancel subscription</button>
+	{:else}
+		<button>Start subscription</button>
+	{/if}
+	<Logout />
+{/if}
---
+++ src/routes/settings/Setting.svelte
@@ -0,0 +1,29 @@
+<script>
+	// @ts-nocheck
+
+	export let key = 'mode';
+	export let data = { mode: 'Dark Mode', sound: 'Regular', motion: 'Regular', data: 'Regular' };
+	export let options = ['Dark Mode', 'Light Mode'];
+
+	let labelMap = {
+		mode: 'Appearance Theme',
+		sound: 'Sound Settings',
+		motion: 'Motion Setings',
+		data: 'Data Usage'
+	};
+
+	function updatePair(newvalue) {
+		data = { ...data, [key]: newvalue };
+		localStorage.setItem('yZgvPlBiFb', data);
+	}
+</script>
+
+<div>
+	{labelMap[key]}:
+</div>
+
+{#each options as option}
+	<button on:click={() => updatePair(option)}
+		>{#if option === data[key]}<b>{option}</b>{:else}{option}{/if}</button
+	>
+{/each}
