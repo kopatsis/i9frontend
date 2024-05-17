3f64355e532121538d42cb920cd2eb7a08178e5f
jwork <jkopatsis@gmail.com>
2024-05-17 11:16:39 -0700

    audio to multiplaylist

--- src/popups/Audio.svelte
+++ src/popups/Audio.svelte
@@ -2,43 +2,64 @@
 	import Modal from '../templates/Modal.svelte';
 
 	export let display = false;
+
+	let displayList = false;
+	let src =
+		'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/40040861&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true';
+	let title = 'Default Playlist';
+
+	const allSrc = [
+		{
+			src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/40040861&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+			title: 'Default Playlist'
+		},
+		{
+			src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/255837157&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+			title: 'Num 2 Playlist'
+		}
+	];
+
+	/**
+	 * @param {string} srcNew
+	 * @param {string} titleNew
+	 */
+	function switchPlaylist(srcNew, titleNew) {
+		src = srcNew;
+		title = titleNew;
+		displayList = false;
+	}
 </script>
 
 <Modal bind:display closeable={false}>
-	<button class={display ? '' : 'hide'} on:click={() => (display = false)}>Hide</button>
+	<div class={display ? '' : 'hide'}>
+		<button on:click={() => (display = false)}>Hide</button>
+		<button on:click={() => (displayList = !displayList)}
+			>{#if displayList}Back to Music{:else}All Playlists{/if}</button
+		>
 
-	<iframe
-		class={display ? '' : 'hide'}
-		title="audioplayer"
-		width="100%"
-		height="300"
-		scrolling="no"
-		frameborder="no"
-		allow="autoplay"
-		src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/40040861&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-	></iframe>
-	<div
-		class={display ? '' : 'hide'}
-		style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"
-	>
-		<a
-			href="https://soundcloud.com/iamtchami"
-			title="Tchami"
-			target="_blank"
-			style="color: #cccccc; text-decoration: none;">Tchami</a
-		>
-		·
-		<a
-			href="https://soundcloud.com/iamtchami/sets/mixes"
-			title="MIXES"
-			target="_blank"
-			style="color: #cccccc; text-decoration: none;">MIXES</a
-		>
+		<div class={!displayList ? '' : 'hide'}>{title}</div>
+		<iframe
+			class={!displayList ? '' : 'hide'}
+			title="audioplayer"
+			width="100%"
+			height="300"
+			scrolling="no"
+			frameborder="no"
+			allow="autoplay"
+			{src}
+		></iframe>
+
+		<div class={displayList ? '' : 'hide'}>
+			Playlist List
+			{#each allSrc as pl}
+				<button on:click={() => switchPlaylist(pl.src, pl.title)}>{pl.title}</button>
+			{/each}
+		</div>
 	</div>
-
-	<style>
-		.hide {
-			display: none;
-		}
-	</style>
 </Modal>
+
+<style>
+	.hide {
+		display: none;
+	}
+</style>
