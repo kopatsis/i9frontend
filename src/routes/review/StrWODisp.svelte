70800801f656573c500610d8c79c18d6419a5615
James Kopatsis <jkopatsis@gmail.com>
2024-05-18 15:00:32 -0700

    gridify to str wo review too

--- src/routes/review/StrWODisp.svelte
+++ src/routes/review/StrWODisp.svelte
@@ -14,56 +14,74 @@
 </script>
 
 <div class="rounddisp">
-	<div class="inner">
-		{#if strRounds}
-			{#if disp === 'Dynamic'}
-				<div>Dynamic Stretch Warmup:</div>
-				{#each strRounds.dynamic.times as time, i}
-					<div>
-						<span>{Math.round(time)}s: &nbsp;</span>
-						<span>{strRounds.dynamic.titles[i]}</span>
-						<button
-							on:click={() => {
-								showCurrentSample(strRounds.dynamic.samples[i]);
-							}}>&#x2139;</button
-						>
-					</div>
-				{/each}
-			{:else}
-				<div>Static Stretch Cooldown:</div>
-				{#each strRounds.static.times as time, i}
-					<div>
-						<span>{Math.round(time)}s: &nbsp;</span>
-						<span>{strRounds.static.titles[i]}</span>
-						<button
-							on:click={() => {
-								showCurrentSample(strRounds.static.samples[i]);
-							}}>&#x2139;</button
-						>
-					</div>
-				{/each}
-			{/if}
-
-			{#if sampleExists}
-				<Sample sampleID={currentSampleID} bind:exists={sampleExists} />
-			{/if}
+	{#if strRounds}
+		{#if disp === 'Dynamic'}
+			<div>Dynamic Stretches:</div>
+			<div class="inner">
+				<div class="grid">
+					{#each strRounds.dynamic.times as time, i}
+						<div>{Math.round(time)}s</div>
+						<div>{strRounds.dynamic.titles[i]}</div>
+						<div>
+							<button
+								on:click={() => {
+									showCurrentSample(strRounds.dynamic.samples[i]);
+								}}>&#x2139;</button
+							>
+						</div>
+					{/each}
+				</div>
+			</div>
+		{:else}
+			<div>Static Stretches:</div>
+			<div class="inner">
+				<div class="grid">
+					{#each strRounds.static.times as time, i}
+						<div>{Math.round(time)}s</div>
+						<div>{strRounds.static.titles[i]}</div>
+						<div>
+							<button
+								on:click={() => {
+									showCurrentSample(strRounds.static.samples[i]);
+								}}>&#x2139;</button
+							>
+						</div>
+					{/each}
+				</div>
+			</div>
 		{/if}
-	</div>
+
+		{#if sampleExists}
+			<Sample sampleID={currentSampleID} bind:exists={sampleExists} />
+		{/if}
+	{/if}
 </div>
 
 <style>
+	.grid {
+		display: grid;
+		grid-template-columns: max-content 1fr max-content;
+		gap: 1px;
+		background-color: rgb(228, 228, 228);
+	}
+	.grid > div {
+		padding: 6px;
+		background: white;
+	}
 	.rounddisp {
 		flex: 1;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 	}
 
-	.inner{
+	.inner {
 		border-radius: 0px;
 		transition: border-color 150ms ease-in-out 0s;
 		border: 1px solid rgb(137, 151, 155);
 		overflow-y: scroll;
 		max-height: 100%;
+		padding: 6px;
 	}
 </style>
