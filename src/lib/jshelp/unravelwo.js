33438c08f3fef96b41066c82f422a68c278dec0b
James Kopatsis <jkopatsis@gmail.com>
2024-07-30 21:28:58 -0700

    unravel takes cycles into account

--- src/lib/jshelp/unravelwo.js
+++ src/lib/jshelp/unravelwo.js
@@ -16,7 +16,6 @@ import {
 } from '$lib/stores/workout';
 
 export function unravelstretchWO(response) {
-
 	wipeWorkout();
 
 	let runningtime = 0.0;
@@ -36,88 +35,103 @@ export function unravelstretchWO(response) {
 		throw new Error('No existing static or dynamic samples');
 	}
 	const strRounds = {
-		static: { samples: workout.StaticSamples, titles: workout.StaticNames, times: [], cycleends: [] },
-		dynamic: { samples: workout.DynamicSamples, titles: workout.DynamicNames, times: [], cycleends: []  }
+		static: {
+			samples: workout.StaticSamples,
+			titles: workout.StaticNames,
+			times: [],
+			cycleends: []
+		},
+		dynamic: {
+			samples: workout.DynamicSamples,
+			titles: workout.DynamicNames,
+			times: [],
+			cycleends: []
+		}
 	};
 	const genTimes = { dynamic: 0, static: 0, end: 0 };
 	const script = [];
 	const timescript = [];
 
-	for (let i = 0; i < dynamicSlice.length; i++) {
-		const set = dynamicSlice[i];
+	for (let h = 0; h < workout.Cycles; h++) {
+		for (let i = 0; i < dynamicSlice.length; i++) {
+			const set = dynamicSlice[i];
 
-		let endcycle = false;
-		if ((i+1) % response.workout.CycleLength === 0) {
-			endcycle = true;
-		}
+			let endcycle = false;
+			if ((i + 1) % response.workout.CycleLength === 0) {
+				endcycle = true;
+			}
 
-		timescript.push({ time: runningtime, isrest: false });
-		strRounds.dynamic.times.push(set.FullTime);
-		strRounds.dynamic.cycleends.push(endcycle);
+			timescript.push({ time: runningtime, isrest: false });
+			strRounds.dynamic.times.push(set.FullTime);
+			strRounds.dynamic.cycleends.push(endcycle);
 
-		for (let j = 0; j < set.RepCount; j++) {
-			const rep = set.RepSlice[set.RepSequence[j]];
+			for (let j = 0; j < set.RepCount; j++) {
+				const rep = set.RepSlice[set.RepSequence[j]];
 
-			for (let k = 0; k < rep.Positions.length; k++) {
-				script.push({
-					time: runningtime,
-					position: rep.Positions[k],
-					set: i + 1,
-					names: [workout.DynamicNames[i]],
-					reps: [0]
-				});
+				for (let k = 0; k < rep.Positions.length; k++) {
+					script.push({
+						time: runningtime,
+						position: rep.Positions[k],
+						set: i + 1,
+						names: [workout.DynamicNames[i]],
+						reps: [0]
+					});
 
-				if (set.SeparateStretch && set.RepSlice.length > 1) {
-					if (set.RepSequence[j] === 0) {
-						script[script.length - 1].names[0] += ' - Left';
-					} else {
-						script[script.length - 1].names[0] += ' - Right';
+					if (set.SeparateStretch && set.RepSlice.length > 1) {
+						if (set.RepSequence[j] === 0) {
+							script[script.length - 1].names[0] += ' - Left';
+						} else {
+							script[script.length - 1].names[0] += ' - Right';
+						}
 					}
-				}
 
-				runningtime += rep.Times[k];
+					runningtime += rep.Times[k];
+				}
 			}
 		}
 	}
 
 	genTimes.static = runningtime;
 
-	for (let i = 0; i < staticSlice.length; i++) {
-		const set = staticSlice[i];
+	for (let h = 0; h < workout.Cycles; h++) {
+		for (let i = 0; i < staticSlice.length; i++) {
+			const set = staticSlice[i];
 
-		let endcycle = false;
-		if ((i+1) % response.workout.CycleLength === 0) {
-			endcycle = true;
-		}
+			let endcycle = false;
+			if ((i + 1) % response.workout.CycleLength === 0) {
+				endcycle = true;
+			}
 
-		timescript.push({ time: runningtime, isrest: false });
-		strRounds.static.times.push(set.FullTime);
-		strRounds.static.cycleends.push(endcycle);
+			timescript.push({ time: runningtime, isrest: false });
+			strRounds.static.times.push(set.FullTime);
+			strRounds.static.cycleends.push(endcycle);
 
-		for (let j = 0; j < set.RepCount; j++) {
-			const rep = set.RepSlice[set.RepSequence[j]];
+			for (let j = 0; j < set.RepCount; j++) {
+				const rep = set.RepSlice[set.RepSequence[j]];
 
-			for (let k = 0; k < rep.Positions.length; k++) {
-				script.push({
-					time: runningtime,
-					position: rep.Positions[k],
-					set: i + 1,
-					names: [workout.StaticNames[i]],
-					reps: [0]
-				});
+				for (let k = 0; k < rep.Positions.length; k++) {
+					script.push({
+						time: runningtime,
+						position: rep.Positions[k],
+						set: i + 1,
+						names: [workout.StaticNames[i]],
+						reps: [0]
+					});
 
-				if (set.RepSlice.length > 1) {
-					if (set.RepSequence[j] === 0) {
-						script[script.length - 1].names[0] += ' - Left';
-					} else {
-						script[script.length - 1].names[0] += ' - Right';
+					if (set.RepSlice.length > 1) {
+						if (set.RepSequence[j] === 0) {
+							script[script.length - 1].names[0] += ' - Left';
+						} else {
+							script[script.length - 1].names[0] += ' - Right';
+						}
 					}
-				}
 
-				runningtime += rep.Times[k];
+					runningtime += rep.Times[k];
+				}
 			}
 		}
 	}
+
 	timescript.push({ time: runningtime, isrest: false });
 	script.push({
 		time: runningtime,
@@ -130,8 +144,8 @@ export function unravelstretchWO(response) {
 	genTimes.end = runningtime;
 
 	if (response.workout.PausedTime !== 0) {
-		currenttimeSet(response.workout.PausedTime*60);
-	} 
+		currenttimeSet(response.workout.PausedTime * 60);
+	}
 
 	strRoundsStSet(strRounds);
 	genTimesStSet(genTimes);
@@ -145,7 +159,6 @@ export function unravelstretchWO(response) {
 
 // @ts-ignore
 export function unravelWO(response, type = 'Regular') {
-
 	wipeWorkout();
 
 	let runningtime = 0.0;
@@ -234,10 +247,12 @@ export function unravelWO(response, type = 'Regular') {
 			start: runningtime
 		});
 
-		if(workoutRounds.length < 2){
+		if (workoutRounds.length < 2) {
 			workoutRounds[0].start -= workout.DynamicRest;
 		} else {
-			workoutRounds[workoutRounds.length-1].start -= (workoutRounds[workoutRounds.length-2].roundrest + workoutRounds[workoutRounds.length-2].off);
+			workoutRounds[workoutRounds.length - 1].start -=
+				workoutRounds[workoutRounds.length - 2].roundrest +
+				workoutRounds[workoutRounds.length - 2].off;
 		}
 
 		if (round.Type == 'Split') {
@@ -249,15 +264,14 @@ export function unravelWO(response, type = 'Regular') {
 		}
 
 		for (let i = 0; i < round.SetCount; i++) {
-
 			const set = round.SetSlice[round.SetSequence[i]];
 
 			runningtime--;
 			script.push({
 				time: runningtime,
 				position: set.PositionInit,
-				set: script[script.length-1].set,
-				names: script[script.length-1].set,
+				set: script[script.length - 1].set,
+				names: script[script.length - 1].set
 			});
 			runningtime++;
 
@@ -333,7 +347,7 @@ export function unravelWO(response, type = 'Regular') {
 	workoutRounds.push({
 		round: 10,
 		sets: 0,
-		type: "",
+		type: '',
 		samples: [],
 		titles: [],
 		reps: [],
@@ -342,7 +356,8 @@ export function unravelWO(response, type = 'Regular') {
 		roundrest: 0,
 		start: runningtime
 	});
-	workoutRounds[workoutRounds.length-1].start -= (workoutRounds[workoutRounds.length-2].roundrest + workoutRounds[workoutRounds.length-2].off);
+	workoutRounds[workoutRounds.length - 1].start -=
+		workoutRounds[workoutRounds.length - 2].roundrest + workoutRounds[workoutRounds.length - 2].off;
 
 	genTimes.static = runningtime;
 
@@ -389,8 +404,8 @@ export function unravelWO(response, type = 'Regular') {
 	genTimes.end = runningtime;
 
 	if (response.workout.PausedTime !== 0) {
-		currenttimeSet(response.workout.PausedTime*60);
-	} 
+		currenttimeSet(response.workout.PausedTime * 60);
+	}
 
 	strRoundsStSet(strRounds);
 	genTimesStSet(genTimes);
